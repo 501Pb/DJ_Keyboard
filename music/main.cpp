b9567447a4a31dc65433010728dd69e9dcd6ef32
Phaskal <hbinl@naver.com>
2019-11-04 22:16:56 +0900

    fix main test
    optimized

--- music/main.cpp
+++ music/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 #include <irrKlang.h>
 #include <queue>
 #include <vector>
@@ -10,21 +11,23 @@ using namespace std;
 
 int main(int argc, const char** argv) {
 	// start the sound engine with default parameters
-	vector<vector<ISoundEngine *>> music(16, vector<ISoundEngine *>(14, NULL));
-	for (int i = 0; i < 16; i++) {
-		for (int j = 0; j < 14; j++) {
-			music[i][j] = createIrrKlangDevice();
-		}
-	}
 
-	vector<queue<int>> note;
+	ISoundEngine * music = createIrrKlangDevice();
+	music->play2D("./music/Do.wav", false);
+	music->play2D("./music/Mi.wav", false);
+	music->play2D("./music/Sol.wav", false);
+	Sleep(500);
+	music->play2D("./music/Le.wav", false);
+	music->play2D("./music/Sol.wav", false);
+	Sleep(500);
+	music->play2D("./music/Si.wav", false);
+	music->play2D("./music/Fa.wav", false);
+	Sleep(500);
+	music->play2D("./music/Do.wav", false);
+	Sleep(500);
 
-
-	music[0][0]->play2D("./music/Do.wav", false);
-	music[0][1]->play2D("./music/Mi.wav", false);
-	music[0][2]->play2D("./music/Sol.wav", false);
 	int i;
 	cin >> i;
-	//music[0][0]->drop();
+	music->drop();
 	return 0;
 }
