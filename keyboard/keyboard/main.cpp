280199fc600e78040de604ec3ac2ab831726e7f9
FKgk <rhkd865@gmail.com>
2019-11-10 13:45:41 +0900

    add filename setting

--- keyboard/keyboard/main.cpp
+++ keyboard/keyboard/main.cpp
@@ -2,23 +2,24 @@
 #include "Recording.h"
 
 int main(void) {
-	game_status status = Init; // 게임 상태 표시
-	std::thread thread_recording(Record, &status); // 녹음용 쓰레드
+	game_status status = game_status::Init; // 게임 상태 표시
+	string filename = "music";	// filename 저장
+	thread thread_recording(Record, &status, &filename); // 녹음용 쓰레드
 	
 	game_init();//게임 초기설정
 	title();//타이틀
 	system("cls");
-
+		
 	
 	do {
-		status = Start;
+		status = game_status::Start;
 		
 		play_game();
 
-		status = GameOver;
+		status = game_status::GameOver;
 	} while (game_over());//게임오버 당할때까지
 	
-	status = End;
+	status = game_status::End;
 	thread_recording.join(); // 쓰레드 종료까지 대기
 
 	return 0;
