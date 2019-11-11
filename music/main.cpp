6e4341a374778c08d486bbee0ba00ee425d02e11
Phaskal <hbinl@naver.com>
2019-11-11 22:31:08 +0900

    Update main.cpp
    clear console update

--- music/main.cpp
+++ music/main.cpp
@@ -3,6 +3,7 @@
 #include <irrKlang.h>
 #include <queue>
 #include <vector>
+#include <stdlib.h>
 
 using namespace irrklang;
 using namespace std;
@@ -11,8 +12,8 @@ using namespace std;
 
 int main(int argc, const char** argv) {
 	// start the sound engine with default parameters
-
 	ISoundEngine * music = createIrrKlangDevice();
+	system("cls");
 	music->play2D("./music/Do.wav", false);
 	music->play2D("./music/Mi.wav", false);
 	music->play2D("./music/Sol.wav", false);
