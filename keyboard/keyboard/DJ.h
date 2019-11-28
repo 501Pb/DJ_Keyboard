5377ec2ef42fa3cb805835aa9fa08816f3d249d0
kwonnaseong <50594187+kwonnaseong@users.noreply.github.com>
2019-11-28 23:00:19 +0900

    Delete title function and Add color

--- keyboard/keyboard/DJ.h
+++ keyboard/keyboard/DJ.h
@@ -23,6 +23,24 @@
 #define SPEED 500 // 단어가 생성되는 간격 (단위 : ms). 조절하여 난이도 증감 가능. 500이하는 플레이 어려움. ns
 #define ONE_SECOND 1000 //1초에 해당하는 1000ms = 1s 
 
+/*콘솔 글자 색상*/
+#define BLACK 0 
+#define BLUE 1 
+#define GREEN 2 
+#define CYAN 3 
+#define RED 4 
+#define MAGENTA 5 
+#define BROWN 6 
+#define LIGHTGRAY 7 
+#define DARKGRAY 8 
+#define LIGHTBLUE 9 
+#define LIGHTGREEN 10 
+#define LIGHTCYAN 11 
+#define LIGHTRED 12 
+#define LIGHTMAGENTA 13 
+#define YELLOW 14 
+#define WHITE 15 
+
 #pragma comment(lib, "winmm.lib") //wav 파일 재생
 
 //#define SOUND_FILE_NAME "../sound/do.wav"
@@ -41,13 +59,10 @@ void random_word(void); //출력될 단어 랜덤으로 설정하는 함수
 void help_function(); // 도움말 출력 함수
 bool menu_function(); //메뉴 출력 함수
 void GotoXY(int x, int y); //커서 이동 함수
+void textcolor(int foreground, int background); //글자 색상 출력 함수
 
-/*입출력 스레드 함수*/
-void* t_function1(void* data); // 스레드화 할 사용자 입력 함수
-void start_thread1(); // 스레드 시작 함수
-void end_thread1(); // 스레드 중지 함수
-
-void* t_function1(void* data); // 스레드화 할 사용자 입력 함수
-void start_thread2(); // 스레드 시작 함수
-void end_thread2(); // 스레드 중지 함수
+/*입력 스레드 함수*/
+void* t_function(void* data); // 스레드화 할 사용자 입력 함수
+void start_thread(); // 스레드 시작 함수
+void end_thread(); // 스레드 중지 함수
 
--- keyboard/keyboard/main.cpp
+++ keyboard/keyboard/main.cpp
@@ -3,9 +3,7 @@
 int main(void) {
 	
 	game_init();//게임 초기설정
-	title();//타이틀
-	system("cls");
-			
+
 	while (1) {
 
 		if (!menu_function()) break; //menu_function()함수가 1을 반환하면 게임 종료함.
